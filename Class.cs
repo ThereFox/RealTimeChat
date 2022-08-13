36c4c0926f148bf0e2e60d80eb30d958a9ade761
ThereFox <sashaassasin2@gmail.com>
2022-08-13 15:28:26 +0400

    Добавьте файлы проекта.

---
+++ Class.cs
@@ -0,0 +1,16 @@
+﻿namespace SignalRTest.MessageDatas
+{
+    public class MessageData
+    {
+        public string SenderName { get; set; }
+        public string Message { get; set; }
+        public DateTime SendDateTime { get; set; }
+        
+        public MessageData(string senderName, string message)
+        {
+            SenderName = senderName;
+            Message = message;
+            SendDateTime = DateTime.Now;
+        }
+    }
+}
